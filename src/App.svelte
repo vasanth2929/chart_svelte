cb03348a239f919724b1105165aebd2e25b6c791
vasanth <vasanth@techardors.com>
2021-01-14 18:35:51 +0530

    feat: bar chart skeleton code

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,5 @@
 <script lang="ts">
-	import RadialChart from "./components/RadialChart.svelte";
-	import type { Data } from "./types";
-	let data: Data<"spent", "estimate"> = [
-		{
-			left: { name: "spent", value: 3 },
-			right: { name: "estimate", value: 5 },
-		},
-		{
-			left: { name: "spent", value: 6 },
-			right: { name: "estimate", value: 3 },
-		},
-		{
-			left: { name: "spent", value: 3 },
-			right: { name: "estimate", value: 8 },
-		},
-	];
+	import Bar from './components/BarChart.svelte';
 </script>
 
-<RadialChart {data} />
+<Bar />
---
+++ src/components/BarChart.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+    let width : number = 550;
+    let height : number = 500 ;
+    let yGap = 50;
+    let xAxisLabels = ['MileStone 1','Milestone 2','Milestone 3','Milestone 4']
+    let yAxisValues = [50,100,150,200,250,300,350,400,450];
+</script>
+
+<div class="container">
+    <svg {width} {height}>
+        <!-- xaxis line -->
+        <rect 
+            width="100%" 
+            height="3" 
+            fill="grey"
+            x="0" 
+            y={height - yGap} />
+        
+        <!-- yaxis line -->
+        <rect 
+          y="0"
+          x="0"
+          width="3"
+          fill="grey"
+          height={height-yGap}
+        />
+    
+        <!-- xaxis label -->
+        {#each xAxisLabels as label,i }
+          <text 
+            x={i * 150 + 20}
+            y={height-yGap+20}
+            >
+             {label}
+            </text>
+            <rect 
+              x={i * 150 + 20}
+              width="30"
+              height="100"
+              y={height-150}
+              fill="red"
+            />
+            <rect 
+              x={i * 150 + 20 +32}
+              width="30"
+              height="100"
+              fill="green"
+              y={height-150}
+            />
+        {/each}
+
+        <!-- xaxis label -->
+        {#each yAxisValues as label,i }
+          <text 
+            class="y-axis-value"
+            x="0"
+            y={height-yGap - i * 50}
+            >
+             {label}
+            </text> 
+        {/each}
+        
+    </svg>
+</div>
+
+<style>
+    svg {
+        overflow: visible;
+    }
+    text {
+        font-size: 12px;
+    }
+    .container {
+        display: flex;
+        justify-content: center;
+        margin-top: 50px;
+    }
+    .y-axis-value {
+        transform: translateX(-25px);
+    }
+</style>
