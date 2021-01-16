c170c7221e6196331f7a4b4e29e0e199323f25cb
vasanth <vasanth@techardors.com>
2021-01-16 09:50:36 +0530

    refactor: folder structure change

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,4 @@
 <script lang="ts">
-	import RadialChart from "./components/RadialChart.svelte";
-	import type { Data } from "./types";
-	let data: Data<"spent", "estimate"> = [
-		{
-			name: 'M1',
-			left: { name: "spent", value: 3 },
-			right: { name: "estimate", value: 5 },
-		},
-		{   
-			name: 'M2',
-			left: { name: "spent", value: 6 },
-			right: { name: "estimate", value: 3 },
-		},
-		{
-			name: 'M3',
-			left: { name: "spent", value: 3 },
-			right: { name: "estimate", value: 8 },
-		},
-	];
+
 </script>
 
-<RadialChart {data} />
