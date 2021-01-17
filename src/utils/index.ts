e233fbc52f14a2123f9c26741fa5fb17b189d6cc
vasanth <vasanth@techardors.com>
2021-01-17 16:13:54 +0530

    fix: findMinAndMax function logic change

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,13 +10,13 @@ export function findMinAndMax(
   items: { left: { value: number }; right: { value: number } }[]
 ): { min: number; max: number } {
   let min, max;
-  min = items[0].left.value;
-  max = items[0].right.value;
+  min = items[0]?.left?.value;
+  max = items[0]?.right?.value;
   for (let item of items) {
-    if (item.left.value < min) min = item.left.value;
-    if (item.right.value < min) min = item.right.value;
-    if (item.left.value > max) max = item.left.value;
-    if (item.right.value > max) max = item.right.value;
+    if (item?.left.value < min) min = item?.left.value;
+    if (item?.right.value < min) min = item?.right.value;
+    if (item?.left.value > max) max = item?.left.value;
+    if (item?.right.value > max) max = item?.right.value;
   }
   return { min, max };
 }
